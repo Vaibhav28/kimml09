4b35f3f7af5fbc0826ecb4187ebdab06a34a91b5
Mattijs Meiboom <m.meiboom@student.rug.nl>
2012-09-25 12:47:25 +0200

    Added __main__ to framework to prevent auto-run after import

--- src/framework.py
+++ src/framework.py
@@ -5,7 +5,7 @@ from models import NaiveBayes
 import scipy.io as sio
 
 # Loadmat should use $PATH, but somehow it fails for me (Mattijs)
-# subject = sio.loadmat('../../fmri_project/data-starplus-04799-ROI.mat')
+#subject = sio.loadmat('../../fmri_project/data-starplus-04799-v7.mat')
 subject = sio.loadmat('data-starplus-04799-v7.mat')
 
 data_wrapper = DataWrapper(subject)
@@ -14,8 +14,9 @@ data_wrapper.extract_values()
 naive_bayes = NaiveBayes(data_wrapper)
 naive_bayes.train()
 
-# Classification starts here
-trial_index = 3
-for scan_index in range(54):
-    scan = subject['data'][trial_index][0][scan_index]
-    naive_bayes.classify(scan, scan_index)
+if __name__ == '__main__':
+    # Classification starts here
+    trial_index = 3
+    for scan_index in range(54):
+        scan = subject['data'][trial_index][0][scan_index]
+        naive_bayes.classify(scan, scan_index)
