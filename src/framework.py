b2d4cf02621e811a086d4f3cf6455c0a75a328bb
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-09-26 11:15:35 +0200

    added average matlab frameworks files

--- src/framework.py
+++ src/framework.py
@@ -4,16 +4,25 @@ from data_wrapper import DataWrapper
 from models import NaiveBayes
 import scipy.io as sio
 
-subject = sio.loadmat('data-starplus-04799-v7.mat')
+files = [
+	'matlab_avergage_rois_04799.mat',
+	# 'matlab_avergage_rois_04820.mat',
+	# 'matlab_avergage_rois_04847.mat'
+]
 
-data_wrapper = DataWrapper(subject)
-data_wrapper.extract_values()
+for file in files:
+	subject = sio.loadmat(file)
 
-naive_bayes = NaiveBayes(data_wrapper)
-naive_bayes.train()
+	data_wrapper = DataWrapper(subject)
+	data_wrapper.extract_values()
 
+	naive_bayes = NaiveBayes(data_wrapper)
+	naive_bayes.train()
+
+
+class_subject = sio.loadmat('matlab_avergage_rois_04799.mat')
 # Classification starts here
 trial_index = 3
 for scan_index in range(54):
-	scan = subject['data'][trial_index][0][scan_index]
+	scan = class_subject['data'][trial_index][0][scan_index]
 	naive_bayes.classify(scan, scan_index)
