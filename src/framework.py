cc78f88033c4ec51ab9cc3dab732590a60eb5426
Mattijs Meiboom <m.meiboom@student.rug.nl>
2012-09-26 11:12:25 +0200

    Added a basic "result" to the output - still need to think about what we classify

--- src/framework.py
+++ src/framework.py
@@ -4,8 +4,7 @@ from data_wrapper import DataWrapper
 from models import NaiveBayes
 import scipy.io as sio
 
-# Loadmat should use $PATH, but somehow it fails for me (Mattijs)
-#subject = sio.loadmat('../../fmri_project/data-starplus-04799-v7.mat')
+# Make sure the *.mat files are in the same directory
 subject = sio.loadmat('data-starplus-04799-v7.mat')
 
 data_wrapper = DataWrapper(subject)
@@ -14,9 +13,16 @@ data_wrapper.extract_values()
 naive_bayes = NaiveBayes(data_wrapper)
 naive_bayes.train()
 
+# Classification starts here
 if __name__ == '__main__':
-    # Classification starts here
     trial_index = 3
-    for scan_index in range(54):
+    score = 0
+    scanset = range(54)  # We probably want only certain scans classified
+    for scan_index in scanset:
         scan = subject['data'][trial_index][0][scan_index]
-        naive_bayes.classify(scan, scan_index)
+        # Obviously, expected should be derived
+        expected_klass = "Sentence"
+        predicted_klass = naive_bayes.classify(scan, scan_index)
+        if expected_klass == predicted_klass:
+            score += 1
+    print "Correctly classified %s out of %s scans" % (score, len(scanset))
