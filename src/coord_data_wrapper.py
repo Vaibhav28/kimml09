413b1d35caceed0affc93688cb6daf806b398e1f
Mattijs Meiboom <m.meiboom@student.rug.nl>
2012-10-02 12:14:32 +0200

    Moved function to separate module, reverted framework, added some notes

--- src/coord_data_wrapper.py
+++ src/coord_data_wrapper.py
@@ -1,57 +1,61 @@
 
 
 class CoordDataWrapper:
-	''''''
+    ''''''
 
-	def __init__(self, subjects):
-		''''''
-		self.subjects = subjects
-		self.features = []
+    def __init__(self, subjects):
+        ''''''
+        self.subjects = subjects
+        self.features = []
 
-	def extract_subject_coords(self, subject):
-		''''''
-		subject_coords = []
-		for z in range(8):
-			for y in range(64):
-				for x in range(64):
-					voxel_index = subject['meta']['coordToCol'][0][0][x][y][z]
-					subject_coords.append(voxel_index)
-		return subject_coords
+    def extract_subject_coords(self, subject):
+        ''''''
+        subject_coords = []
+        for z in range(8):
+            for y in range(64):
+                for x in range(64):
+                    voxel_index = subject['meta']['coordToCol'][0][0][x][y][z]
+                    subject_coords.append(voxel_index)
+        return subject_coords
 
-	def extract_values(self):
-		''''''
-		# features = []
-		# subjects_coords = []
-		# for subject_index in range(len(self.subjects)):
-		# 	subject_coord = []
-		# 	for z in range(8):
-		# 		for y in range(64):
-		# 			for x in range(64):
-		# 				subject_coord.append(self.subjects[subject_index]['meta']['coordToCol'][0][0][x][y][z])
-		# 	subjects_coords.append(subject_coord)
-		subjects_coords = []
-		for subject in self.subjects:
-			subjects_coords.append(self.extract_subject_coords(subject))
+    def extract_values(self):
+        ''''''
+        # features = []
+        # subjects_coords = []
+        # for subject_index in range(len(self.subjects)):
+        # 	subject_coord = []
+        # 	for z in range(8):
+        # 		for y in range(64):
+        # 			for x in range(64):
+        # 				subject_coord.append(self.subjects[subject_index]['meta']['coordToCol'][0][0][x][y][z])
+        # 	subjects_coords.append(subject_coord)
+        subjects_coords = []
+        for subject in self.subjects:
+            subjects_coords.append(self.extract_subject_coords(subject))
 
-		voxel_indexes = []
-		for coord_index in range(32768):
-			something = []
-			for subject_index in range(len(self.subjects)):
-				something.append(subjects_coords[subject_index][coord_index])
-			voxel_indexes.append(something)
+        voxel_indexes = []
+        for coord_index in range(32768):
+            something = []
+            for subject_index in range(len(self.subjects)):
+                something.append(subjects_coords[subject_index][coord_index])
+            voxel_indexes.append(something)
 
-		valid_coords = []
-		for coord in voxel_indexes:
-			if not 0 in coord:
-				valid_coords.append(coord)
+        valid_coords = []
+        for coord in voxel_indexes:
+            if not 0 in coord:
+                valid_coords.append(coord)
 
-		# now we have all the valid voxel indexes for each subject
-		# the valid_coords contains lists (605), every list is a coordinate
-		# which contains the 6 valid voxel indexes one for each subject
-		for i in range(len(self.subjects)):
-			internal = []
-			for j in range(len(valid_coords)):
-				internal.append(valid_coords[j][i])
-			self.features.append(internal)
+        # testing against ROI
+        for coords in valid_coords:
+            for subject_index, coord in enumerate(coords):
+                print self.subjects[subject_index]['meta']['colToROI'][0][0][coord]
+            print '-----'
 
-		
+        # now we have all the valid voxel indexes for each subject
+        # the valid_coords contains lists (605), every list is a coordinate
+        # which contains the valid voxel indexes one for each subject
+        for i in range(len(self.subjects)):
+            internal = []
+            for j in range(len(valid_coords)):
+                internal.append(valid_coords[j][i])
+            self.features.append(internal)
--- src/framework.py
+++ src/framework.py
@@ -8,64 +8,44 @@ accompanying this framework.
 
 @since September 2012
 '''
+import scipy.io as sio
+
 from data_wrapper import DataWrapper
 from coord_data_wrapper import CoordDataWrapper
 from models import NaiveBayes
-import scipy.io as sio
-import sys
+from functions import get_expected_class
+from data_wrapper import FIRST_STIMULUS_SCAN
+from data_wrapper import SECOND_STIMULUS_SCAN
 
+# List of matlab data files
 subjects = [
-    # sio.loadmat('../matlab/matlab_avergage_rois_04799.mat'),
-    # sio.loadmat('../matlab/matlab_avergage_rois_04820.mat'),
-    # sio.loadmat('../matlab/matlab_avergage_rois_04847.mat'),
-    sio.loadmat('../matlab/data-starplus-04799-v7.mat'),
-    sio.loadmat('../matlab/data-starplus-04820-v7.mat'),
-    sio.loadmat('../matlab/data-starplus-04847-v7.mat'),
-    sio.loadmat('../matlab/data-starplus-05675-v7.mat'),
-    sio.loadmat('../matlab/data-starplus-05680-v7.mat'),
+    sio.loadmat('../matlab/matlab_avergage_rois_04799.mat'),
+    sio.loadmat('../matlab/matlab_avergage_rois_04820.mat'),
+    sio.loadmat('../matlab/matlab_avergage_rois_04847.mat'),
+    sio.loadmat('../matlab/matlab_avergage_rois_05675.mat'),
+    sio.loadmat('../matlab/matlab_avergage_rois_05680.mat'),
+    # sio.loadmat('../matlab/matlab_avergage_rois_05710.mat'),
+    # sio.loadmat('../matlab/data-starplus-04799-v7.mat'),
+    # sio.loadmat('../matlab/data-starplus-04820-v7.mat'),
+    # sio.loadmat('../matlab/data-starplus-04847-v7.mat'),
+    # sio.loadmat('../matlab/data-starplus-05675-v7.mat'),
+    # sio.loadmat('../matlab/data-starplus-05680-v7.mat'),
     # sio.loadmat('../matlab/data-starplus-05710-v7.mat')
 ]
 
-coord_data_wrapper = CoordDataWrapper(subjects)
-coord_data_wrapper.extract_values()
+# coord_data_wrapper = CoordDataWrapper(subjects)
+# coord_data_wrapper.extract_values()
 
-data_wrapper = DataWrapper(subjects, coord_data_wrapper)
+data_wrapper = DataWrapper(subjects)
 data_wrapper.extract_values()
 
 naive_bayes = NaiveBayes(data_wrapper)
 naive_bayes.train()
 
-
-def get_expected_class(subject, trial_index, scan_index):
-    '''
-    Checks the subject's info to see what class is expected when
-    classifying a scan from a certain trial. Trial index runs from 1 to 54.
-    Since we only know 'firstStimulus' for a trial, we'll map all scans before
-    [...] to firstStimulus and the rest to the secondStimulus.
-
-    According to data, trial_index 1..27 all have firstStimulus = 'P'.
-    Each trial has ~54 scans, so we'll asume that 27 is the halfwayPoint/
-
-    Returns either 'Picture' or 'Sentence'
-    '''
-    halfwayPoint = 27
-    first_stimulus = subject['info'][0][trial_index]['firstStimulus'][0]
-    if first_stimulus == 'P':
-        if scan_index < halfwayPoint:
-            return 'Picture'
-        else:
-            return 'Sentence'
-    else:
-        if scan_index < halfwayPoint:
-            return 'Sentence'
-        else:
-            return 'Picture'
-
-from data_wrapper import FIRST_STIMULUS_SCAN
-from data_wrapper import SECOND_STIMULUS_SCAN
-
-# Classification starts here
 if __name__ == '__main__':
+    '''
+    From here on, we start classification
+    '''
     class_subject = sio.loadmat('../matlab/matlab_avergage_rois_05710.mat')
     num_of_trials = class_subject['meta']['ntrials'][0][0][0][0]
     valid_trials = data_wrapper.get_valid_trial_indexes(class_subject, num_of_trials)
