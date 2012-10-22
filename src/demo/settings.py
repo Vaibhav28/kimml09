449aa95b492dd67fbc55a7699a7d6002f95e184a
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-10-22 10:48:17 +0200

    changes

--- src/demo/settings.py
+++ src/demo/settings.py
@@ -1,4 +1,6 @@
 FILES = {
+	'Orig': 'data%d-original.mat',
+	'AvgROI': 'data%d_average_rois.mat',
     'NormAvgROI': './matlab/data%d-select-norm-avgroi.mat',
     'NormAvgSDROI': './matlab/data%d-select-norm-avgsdroi.mat',
     'NormPerVoxAvgROI': './matlab/data%d-select-normpervox-avgroi.mat',
